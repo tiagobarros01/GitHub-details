4ed7fefd4e3f5f0c9c490cc4a303cf863932fbd9
tiagobarros01 <tbarros1337@hotmail.com>
2021-07-01 17:12:54 -0300

    chore: Creates a _document

---
+++ src/pages/_document.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Document, {
+  DocumentProps,
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from 'next/document';
+
+class MyDocument extends Document<DocumentProps> {
+  render(): JSX.Element {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
