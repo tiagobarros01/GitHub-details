fcdc96e3f4a000c67edcd41dc3554ba0c43c41d1
tiagobarros01 <tbarros1337@hotmail.com>
2021-07-04 13:34:13 -0300

    style: styling /

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,52 @@
-import { Heading } from '@chakra-ui/react';
+import { Heading, Grid, Flex, Input, Box, Button } from '@chakra-ui/react';
 
 export default function Home() {
   return (
-    <Heading>
-      Hello word
-    </Heading>
-  )
+    <Grid
+      as="main"
+      height="100vh"
+      templateColumns="1fr 380px 1fr"
+      templateRows="1fr 380px 1fr"
+      templateAreas="
+        '. . .'
+        '. form .'
+        '. . .'
+      "
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Flex
+        gridArea="form"
+        height="100%"
+        alignItems="center"
+        justifyContent="center"
+        flexDirection="column"
+        padding="15px"
+      >
+        <Heading size="lg">GitHub Details</Heading>
+        <Box
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          flexDirection="row"
+          marginTop="15px"
+        >
+          <Input
+            backgroundColor="gray.800"
+            focusBorderColor="purple.500"
+            borderTopRightRadius="none"
+            borderBottomRightRadius="none"
+            placeholder="Name of user"
+            />
+          <Button
+            backgroundColor="purple.500"
+            borderRadius="none"
+            _hover={{ backgroundColor: 'purple.600' }}
+          >
+            Search
+          </Button>
+        </Box>
+      </Flex>
+    </Grid>
+  );
 }
