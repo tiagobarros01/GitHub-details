e18fe864cdc0950ce2a73ee2ab7667f6e7929954
tiagobarros01 <tbarros1337@hotmail.com>
2021-07-06 19:45:39 -0300

    feat: Takes value of input & search an user if exists

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,22 @@
 import { Heading, Grid, Flex, Input, Box, Button } from '@chakra-ui/react';
+import { useRef } from 'react';
 
 export default function Home() {
+  const userName = useRef<HTMLInputElement>(null);
+
+  async function getUserData() {
+    const value = userName.current?.value;
+
+    try {
+      const res = await fetch(`https://api.github.com/users/${value}`);
+      const data = await res.json();
+
+      console.log(data);
+    } catch (error) {
+      console.debug(error);
+    }
+  }
+
   return (
     <Grid
       as="main"
@@ -14,6 +30,7 @@ export default function Home() {
       "
       justifyContent="center"
       alignItems="center"
+      backgroundColor="gray.800"
     >
       <Flex
         gridArea="form"
@@ -23,7 +40,9 @@ export default function Home() {
         flexDirection="column"
         padding="15px"
       >
-        <Heading size="lg">GitHub Details</Heading>
+        <Heading size="lg" color="gray.300">
+          GitHub Details
+        </Heading>
         <Box
           display="flex"
           alignItems="center"
@@ -32,16 +51,19 @@ export default function Home() {
           marginTop="15px"
         >
           <Input
+            color="gray.300"
             backgroundColor="gray.800"
             focusBorderColor="purple.500"
             borderTopRightRadius="none"
             borderBottomRightRadius="none"
             placeholder="Name of user"
-            />
+            ref={userName}
+          />
           <Button
             backgroundColor="purple.500"
             borderRadius="none"
             _hover={{ backgroundColor: 'purple.600' }}
+            onClick={getUserData}
           >
             Search
           </Button>
