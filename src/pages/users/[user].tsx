8bf830a1b461fc62019159776fd7192079a35e36
tiagobarros01 <tbarros1337@hotmail.com>
2021-07-08 01:47:20 -0300

    feat: Shows user data on screen

--- src/pages/users/[user].tsx
+++ src/pages/users/[user].tsx
@@ -1,19 +1,94 @@
+import {
+  Heading,
+  Grid,
+  Flex,
+  Box,
+  Button,
+  Image,
+  Text,
+} from '@chakra-ui/react';
 import { useRouter } from 'next/router';
 import { useContext, useEffect } from 'react';
 import { Loader } from '../../components/Loader';
 import { UserContext } from '../../contexts/UserContext';
 
 export default function User() {
-  const { query: { user }} = useRouter();
-  const { isLoading, getUserData, userData } = useContext(UserContext)
+  const {
+    query: { user },
+  } = useRouter();
+  const { isLoading, getUserData, userData } = useContext(UserContext);
 
   useEffect(() => {
-    getUserData(String(user))
+    getUserData(String(user));
   }, [getUserData, user]);
 
   return (
-    <div>
-      {isLoading ? <Loader /> : <h1>{userData.name}</h1>}
-    </div>
-  )
+    <Grid
+      as="main"
+      height="100vh"
+      backgroundColor="gray.800"
+      templateColumns="1fr 460px 1fr"
+      templateRows="1fr 300px 1fr"
+      templateAreas="
+        '. . .'
+        '. box .'
+        '. . . '
+      "
+      justifyContent="center"
+      alignItems="center"
+    >
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <Flex
+          gridArea="box"
+          boxShadow="0 2px 12px rgba(0, 0, 0, 0.4)"
+          height="100%"
+          padding="15px"
+          justifyContent="space-between"
+        >
+          <Box>
+            <Image
+              boxSize="16"
+              borderRadius="full"
+              src={userData.avatar}
+              alt={userData.name}
+            />
+            <Heading color="gray.300" fontSize="2xl" marginTop="10px">
+              {userData.name}
+            </Heading>
+            <Text color="gray.400" fontSize="large">
+              {userData.userName}
+            </Text>
+          </Box>
+          <Box>
+            <Flex>
+              <Text color="gray.400" marginRight="5px" cursor="pointer">
+                {userData.repos}
+              </Text>
+              <Text color="gray.300" fontSize="md">
+                Repos
+              </Text>
+            </Flex>
+            <Flex>
+              <Text color="gray.400" marginRight="5px">
+                {userData.followers}
+              </Text>
+              <Text color="gray.300" fontSize="md">
+                Followers
+              </Text>
+            </Flex>
+            <Flex>
+              <Text color="gray.400" marginRight="5px">
+                {userData.following}
+              </Text>
+              <Text color="gray.300" fontSize="md">
+                Following
+              </Text>
+            </Flex>
+          </Box>
+        </Flex>
+      )}
+    </Grid>
+  );
 }
